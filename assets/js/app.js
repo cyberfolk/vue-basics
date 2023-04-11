f99c4d7e50c40745f5b6cbdf6e9a93f33b71f478
cyberfolk <andrea1995ferrari.com>
2023-04-11 14:25:57 +0200

    Show 10 email getting from API

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,24 +6,32 @@
  * Far comparire gli indirizzi email solamente quando sono stati tutti generati.
  */
 
+const N_MAIL = 10;
 const { createApp } = Vue
 
 createApp({
     data() {
         return {
             url: "https://flynn.boolean.careers/exercises/api/random/mail",
-
+            emails: [],
         }
     },
     methods: {
-        getEmail() {
-            axios
-                .get(this.url)
-                .then(response => {
-                    this.my_data = response.data
-                    console.log(response.data);
-                    console.log(response.data.response)
-                }).catch(error => { console.error(error.message) })
-        }
+        getEmails() {
+            for (let i = 0; i < N_MAIL; i++) {
+                axios
+                    .get(this.url)
+                    .then(response => {
+                        const email = response.data.response
+                        this.emails.push(email);
+                    }).catch(error => { console.error(error.message) })
+            }
+        },
+    },
+    mounted() {
+        this.getEmails();
     }
+
+
+
 }).mount('#app')
